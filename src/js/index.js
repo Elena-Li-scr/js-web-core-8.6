8d57138da53a0cc6ba641924c8c2447e1772c8e8
Elen <lielena412@gmail.com>
2025-02-22 23:59:25 +0900

    third commit

--- src/js/index.js
+++ src/js/index.js
@@ -51,14 +51,52 @@ buttonHide2.addEventListener('click', hide2)
 
 function showMenu() {
   document.querySelector('.left').style.display = 'block'
+  document.querySelector('.menu-cover').style.display = 'block'
 }
 let showMenuButton = document.querySelector('.show-menu')
 showMenuButton.addEventListener('click', showMenu)
 
 function closeMenu() {
   document.querySelector('.left').style.display = 'none'
+  document.querySelector('.menu-cover').style.display = 'none'
 }
 let closeMenuButton = document.querySelector('.close-menu')
 let closeMenuArea = document.querySelector('.menu-cover')
 closeMenuButton.addEventListener('click', closeMenu)
 closeMenuArea.addEventListener('click', closeMenu)
+
+function openMailMenu() {
+  document.querySelector('.menu-form').style.display = 'block'
+  document.querySelector('.menu-cover').style.display = 'block'
+  document.querySelector('.main-menu').style.zIndex = '1'
+}
+
+let openMail = document.querySelector('.icons--open-mail')
+openMail.addEventListener('click', openMailMenu)
+
+function closeMailMenu() {
+  document.querySelector('.menu-form').style.display = 'none'
+  document.querySelector('.menu-cover').style.display = 'none'
+  document.querySelector('.main-menu').style.zIndex = '3'
+}
+
+let closeMail = document.querySelector('.close-mail')
+closeMail.addEventListener('click', closeMailMenu)
+
+function openPhoneMenu() {
+  document.querySelector('.menu-form-phone').style.display = 'block'
+  document.querySelector('.menu-cover').style.display = 'block'
+  document.querySelector('.main-menu').style.zIndex = '1'
+}
+
+let openPhone = document.querySelector('.icons--open-phone')
+openPhone.addEventListener('click', openPhoneMenu)
+
+function closePhoneMenu() {
+  document.querySelector('.menu-form-phone').style.display = 'none'
+  document.querySelector('.menu-cover').style.display = 'none'
+  document.querySelector('.main-menu').style.zIndex = '3'
+}
+
+let closePhone = document.querySelector('.close-phone')
+closePhone.addEventListener('click', closePhoneMenu)
