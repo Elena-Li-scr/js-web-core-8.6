26d190c13569f03e1d6c85e082e5a845f8642481
Elen <lielena412@gmail.com>
2025-02-22 17:37:48 +0900

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,64 @@
 import '../scss/style.scss'
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 
-console.log('It works!')
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      slidesPerView: 'auto',
+      modules: [Navigation, Pagination],
+
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    })
+  }
+})
+function show() {
+  document.querySelector('.swiper').style.height = 'auto'
+  document.querySelector('.show').style.display = 'none'
+  document.querySelector('.hide').style.display = 'flex'
+}
+let buttonShow = document.querySelector('.show')
+buttonShow.addEventListener('click', show)
+function hide() {
+  document.querySelector('.swiper').style.height = '185px'
+  document.querySelector('.show').style.display = 'flex'
+  document.querySelector('.hide').style.display = 'none'
+}
+
+let buttonHide = document.querySelector('.hide')
+buttonHide.addEventListener('click', hide)
+
+function show2() {
+  document.querySelector('.swiper2').style.height = 'auto'
+  document.querySelector('.show2').style.display = 'none'
+  document.querySelector('.hide2').style.display = 'flex'
+}
+let buttonShow2 = document.querySelector('.show2')
+buttonShow2.addEventListener('click', show2)
+function hide2() {
+  document.querySelector('.swiper2').style.height = '185px'
+  document.querySelector('.show2').style.display = 'flex'
+  document.querySelector('.hide2').style.display = 'none'
+}
+
+let buttonHide2 = document.querySelector('.hide2')
+buttonHide2.addEventListener('click', hide2)
+
+function showMenu() {
+  document.querySelector('.left').style.display = 'block'
+}
+let showMenuButton = document.querySelector('.show-menu')
+showMenuButton.addEventListener('click', showMenu)
+
+function closeMenu() {
+  document.querySelector('.left').style.display = 'none'
+}
+let closeMenuButton = document.querySelector('.close-menu')
+let closeMenuArea = document.querySelector('.menu-cover')
+closeMenuButton.addEventListener('click', closeMenu)
+closeMenuArea.addEventListener('click', closeMenu)
